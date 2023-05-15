575096dd002971d6f14f088fa78acffd99b2a314
ondanje <cyrilondanje@gmail.com>
2023-05-15 13:20:46 +0300

    removing specific character from a list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    new_string = ""
-
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-
-    return new_string
+    new_list = list(my_string)
+    for char in new_list:
+        if char == 'c' or char == 'C':
+            new_list.remove(char)
+    return("".join(new_list))
