fe0ef242d2724c958bf2c4d112cdfa9b13929993
ondanje <cyrilondanje@gmail.com>
2023-05-31 00:19:49 +0300

    square with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """a square"""
+
+    def __init__(self, size):
+        """initialize by size
+
+        Args:
+        size(int): size of the square
+        """
+
+        self.__size = size
