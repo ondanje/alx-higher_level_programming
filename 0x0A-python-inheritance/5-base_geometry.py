6afe37b9ab6a6f0298fdf506cc7fc7a86eeaa9a7
ondanje <cyrilondanje@gmail.com>
2023-06-12 23:52:52 +0300

    python inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module of an empty class called BaseGeometry"""
+
+
+class BaseGeometry():
+    """An empty class"""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+
+    def area(self):
+        """area method"""
+
+        raise Exception("area() is not implemented")
