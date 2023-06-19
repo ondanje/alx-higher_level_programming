369e740493aa9e42a599d8fbc3f77720f21879eb
ondanje <cyrilondanje@gmail.com>
2023-06-20 02:29:22 +0300

    tests

--- 0x0C-python-almost_a_circle/tests/test_modules/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_modules/test_base.py
@@ -5,7 +5,6 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
-#from models.square import Square
 
 
 class Test_Base_Create(unittest.TestCase):
@@ -19,6 +18,7 @@ class Test_Base_Create(unittest.TestCase):
         self.assertEqual(str(r1), str(r2))
         self.assertIsNot(r1, r2)
         self.assertNotEqual(r1, r2)
+
 
 if __name__ == '__main__':
     unittest.main()
