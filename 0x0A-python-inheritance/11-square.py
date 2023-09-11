2020cf8b6ab37f9217e9e792418b50c1d992d960
ondanje <cyrilondanje@gmail.com>
2023-09-12 00:21:14 +0300

    python inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+class Square that inherits from Rectangle (9-rectangle.py)
+(task based on 10-square.py)
+"""
+
+
+class Square(Rectangle):
+    """class square"""
+
+    def __init__(self, size):
+
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calculate the area"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
