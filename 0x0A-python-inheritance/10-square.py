432e80c8568b15b54fbad847008f7cfa8a45c91c
ondanje <cyrilondanje@gmail.com>
2023-09-12 00:53:58 +0300

    task 10 correction

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""
+class Square that inherits from Rectangle (9-rectangle.py):
+"""
 Rectangle = __import__('9-rectangle').Rectangle
-"""class Square that inherits from Rectangle (9-rectangle.py):"""
 
 
 class Square(Rectangle):
