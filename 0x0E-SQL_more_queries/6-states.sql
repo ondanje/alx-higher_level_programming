4c4818e75b0d92d80fc9e9aa3c916e8288b0f76b
ondanje <cyrilondanje@gmail.com>
2023-10-18 23:51:09 +0300

    creates a new database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,9 @@
 --creates the database hbtn_0d_usa and the table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- set database
+USE hbtn_0d_usa;
+-- name VARCHAR(256) can’t be null
 -- states (in the database hbtn_0d_usa) on your MySQL server.
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-	id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL,
-	name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL, name VARCHAR(256) NOT NULL);
