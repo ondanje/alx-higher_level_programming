8921e859353307ea318e6c982f089a7947a712cb
ondanje <cyrilondanje@gmail.com>
2023-10-18 23:20:56 +0300

    creating a user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates a user user_0d_1 in the MySQL server
+-- user_0d_1 password should be set to user_0d_1_pwd
+-- If the user user_0d_1 already exists, your script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL  ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
