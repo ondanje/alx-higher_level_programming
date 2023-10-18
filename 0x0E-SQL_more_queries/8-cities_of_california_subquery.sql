33c5bb4edce44c64cf09625bc5f3800a14fd13f1
ondanje <cyrilondanje@gmail.com>
2023-10-18 23:53:43 +0300

    subqueries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,8 +5,7 @@
 --You are not allowed to use the JOIN keyword
 SELECT id, name FROM cities
 WHERE state_id = (
-	SELECT id
-	FROM states
-	WHERE name = 'California'
-)
-ORDER BY cities.id;
+      SELECT id
+      FROM states
+      WHERE name = 'California'
+);
