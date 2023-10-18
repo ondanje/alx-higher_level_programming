deb802106aaffd14e4a882b967c71b0c67c875cc
ondanje <cyrilondanje@gmail.com>
2023-10-18 23:40:31 +0300

    creates a new database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,8 +3,9 @@
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL ,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT UNIQUE NOT NULL ,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(id)
+	PRIMARY KEY (id)
 );
