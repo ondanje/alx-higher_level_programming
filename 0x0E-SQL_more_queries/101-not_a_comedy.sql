cb538f10a34ecf747a051ef691ca935a31c85de4
ondanje <cyrilondanje@gmail.com>
2023-10-22 23:52:11 +0300

    advanced task

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 --a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 --The tv_genres table contains only one record where name = Comedy (but the id can be different)
 --Each record should display: tv_shows.title
---Results must be sorted in ascending order by the show title
+--Results must be sorted in ascending order by show title
 SELECT tv_shows.title FROM tv_shows
 LEFT JOIN (
     SELECT tv_shows.id
