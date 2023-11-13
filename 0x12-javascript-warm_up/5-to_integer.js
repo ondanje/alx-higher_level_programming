bd09d598f1f8808221fc1be6c031ef885b21ce88
ondanje <cyrilondanje@gmail.com>
2023-11-13 19:23:57 +0300

    javascript warmup

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// converting ti integer
+
+const firstArgument = process.argv[2];
+const parsedInteger = parseInt(firstArgument);
+
+if (!isNaN(parsedInteger)) {
+  console.log(`My number: ${parsedInteger}`);
+} else {
+  console.log('Not a number');
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// array and loops in javascript
+
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i <= 2; i++) {
+  console.log(strings[i]);
+}
