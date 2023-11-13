998891379cfd75161042ea0dab40852f7a525d00
ondanje <cyrilondanje@gmail.com>
2023-11-13 18:50:49 +0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 // printing arguments
 
-const numOfArgs = process.argv;
-
-if (numOfArgs.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (numOfArgs.length === 3) {
-  console.log('Arguments found');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
