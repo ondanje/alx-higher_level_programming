ceb8e2bd38c33799ab5517bd48db36fba2db20b1
ondanje <cyrilondanje@gmail.com>
2023-11-13 18:58:00 +0300

    concat strings

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 // concat two string arguments
 
-const numOfArgs = process.argv;
-
-if (numOfArgs[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(numOfArgs[2] + ' is ' + numOfArgs[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
