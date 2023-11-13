b8ca45cb5ef1f56387fa5d9699cd442d42447473
ondanje <cyrilondanje@gmail.com>
2023-11-13 18:56:05 +0300

    concat strings

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -6,5 +6,5 @@ const numOfArgs = process.argv;
 if (numOfArgs[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(numOfArgs[2], 'is', numOfArgs[3]);
+  console.log(numOfArgs[2] + ' is ' + numOfArgs[3]);
 }
