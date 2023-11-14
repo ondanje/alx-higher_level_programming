a0cc41ac3fe374218cf403c1810948ff38577c58
ondanje <cyrilondanje@gmail.com>
2023-11-15 01:25:08 +0300

    task 6 correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 
 const square = require('./5-square');
 class Square extends square {
-  constructor (side) {
-    super(side, side);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
