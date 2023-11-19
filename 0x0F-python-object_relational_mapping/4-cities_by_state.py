b7e538482092bddb21bd04889bd65daf09057860
ondanje <cyrilondanje@gmail.com>
2023-11-19 15:25:07 +0300

    implementing inner joins

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,10 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM cities ORDER BY id")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+        FROM cities INNER JOIN states\
+             ON cities.state_id=states.id\
+                 ORDER BY cities.id;")
 
     rows = cur.fetchall()
 
