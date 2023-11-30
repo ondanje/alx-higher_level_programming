2970d0747fe014d6d12c286ce5da7aef00ee4de8
ondanje <cyrilondanje@gmail.com>
2023-11-30 12:26:18 -0800

    python network 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request
+# A Bash script that takes in a URL, sends a request
 # to that URL, and displays the size of the body of the response
 
 curl -sI $1 | grep "Content-Length" | cut -d " " -f2
