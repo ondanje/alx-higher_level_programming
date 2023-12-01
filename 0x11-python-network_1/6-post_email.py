d29c7b41a8fd4c588975fce89ca0744eff147fb8
ondanje <cyrilondanje@gmail.com>
2023-12-02 01:31:36 +0300

    posting using requests library

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter, and finally
+displays the body of the response.
+"""
+import sys
+import requests
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = {'email': email}
+
+try:
+    response = requests.post(url, data=data)
+
+    print(response.text)
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
