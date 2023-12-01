aff39ea19c974ed1a11d4b3bbe93fd791667b991
ondanje <cyrilondanje@gmail.com>
2023-12-02 00:05:39 +0300

    display error code using requests package

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
 
     response = requests.get(url)
 
-    if response.status_code >= 400 :
+    if response.status_code >= 400:
         print("Error code:", response.status_code)
