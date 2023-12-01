1b3d0cd7acca40c510b31b47a9f68694ece31b73
ondanje <cyrilondanje@gmail.com>
2023-12-02 01:52:45 +0300

    python network 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,23 +14,19 @@ import requests
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         q = ""
-    else :
+    else:
         q = sys.argv[1]
 
     url = 'http://0.0.0.0:5000/search_user'
     data = {'q': q}
 
+    content = requests.post(url, data=data)
+
     try:
-        response = requests.post(url, data=data)
-
-        try:
-            json_response = response.json()
-
-            if json_response:
-                print('[{}] {}'.format(json_response['id'], json_response['name']))
-            else:
-                print('No result')
-        except ValueError:
-            print('Not a valid JSON')
-    except requests.exceptions.RequestException as e:
-        print(f'Error: {e}')
+        response = content.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
