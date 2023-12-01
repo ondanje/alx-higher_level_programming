6180736559ae430615cf86d0c4272403628e767a
ondanje <cyrilondanje@gmail.com>
2023-12-02 01:40:47 +0300

    posting using urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,12 +7,13 @@ email as a parameter, and displays the body of the response
 from urllib import request, parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = parse.urlencode({'email': email}).encode('ascii')
+    data = parse.urlencode({'email': email}).encode('utf-8')
 
-with request.urlopen(url, data=data) as response:
-    content = response.read().decode('utf-8')
+    with request.urlopen(url, data=data) as response:
+        content = response.read().decode('utf-8')
 
-    print(content)
+        print(content)
