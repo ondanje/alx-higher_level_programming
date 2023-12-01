18d73b9cb588220d65f58e5c2319b4a6e116c789
ondanje <cyrilondanje@gmail.com>
2023-12-02 01:30:53 +0300

    posting using urllib

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST requestto the passed URL with the
+email as a parameter, and displays the body of the response
+"""
+from urllib import request, parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = parse.urlencode({'email': email}).encode('utf-8')
+
+with request.urlopen(url, data=data) as response:
+    content = response.read().decode('utf-8')
+
+    print(content)
